3de69027308bbd0c80c6b142a790e4f2ac68703d
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2023-03-08 20:03:44 +0500

    add: adding models js

--- src/model/data.sql
+++ src/model/data.sql
@@ -1,1 +1,3 @@
 insert into admins(username, hash) values ('admin', 'hello123');
+
+insert into news(heading, news_text) values('lorem ipsum', 'text text lorem some text name');
--- src/model/mock.sql
+++ src/model/mock.sql
@@ -6,4 +6,13 @@ create table admins(
      hash text not null
 );
 
+SELECT uuid_generate_v4();
+
+
+create table news(
+     id uuid default uuid_generate_v4(),
+     heading varchar(50) not null,
+     news_text text not null,
+     date date default current_date
+); 
 
