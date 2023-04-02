4234b807eeb817c7b932ca7ab8f707b6aa66568b
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2023-04-02 15:52:13 +0500

    add: adding teachers module files

--- src/model/mock.sql
+++ src/model/mock.sql
@@ -44,3 +44,17 @@ create table contact(
 --      fullName text not null,
 
 -- );
+
+
+create table teachers(
+     id uuid default uuid_generate_v4(),
+     fullName varchar(100) not null,
+     lavozim text not null,
+     birth text not null,
+     birthPlace text not null,
+     millat text not null,
+     partiyaviyligi text not null,
+     malumoti text not null,
+     talim text not null,
+     photo text not null
+);
