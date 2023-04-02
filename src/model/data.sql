cb38301a394a49855887b05daac7567b5634e038
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2023-04-02 16:01:40 +0500

    add: adding teachers table and some datas

--- src/model/data.sql
+++ src/model/data.sql
@@ -1,4 +1,4 @@
-insert into admins(fullName, lavozim, birth, birthPlace, millat, partiyaviyligi, malumoti, talim, photo) values('Abdraxmanova Sayyora Imom Alievna', 'Maktab direktori', '2.03.1963', 'Qozog`iston respublikasi, Chimkent shahri', 'o`zbek', 'yo`q', 'oliy', '1985 y. Chimkent davlat pedagogika instituti (kunduzgi)', 'https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/profile-photos-4.jpg');
+insert into admins(fullName, lavozim, birth, birthPlace, millat, partiyaviyligi, malumoti, talim, photo) values('Abdraxmanova Sayyora Imom Alievna', 'Maktab direktori', '2.03.1963', 'Qozog`iston respublikasi, Chimkent shahri', 'o`zbek', 'yo`q', 'oliy', '1985 y. Chimkent davlat pedagogika instituti (kunduzgi)', 'https://telegra.ph/file/2fac8c92393e6f5311e7e.jpg');
 
 
 insert into news (heading, text, img1, img2) values ('Matsoft', 'hello world, that''s a beautiful day', 'http://dummyimage.com/350x250.png/5fa2dd/ffffff', 'http://dummyimage.com/350x250.png/ff4444/ffffff');
@@ -9,4 +9,4 @@ insert into news (heading, text, img1, img2) values ('Holdlamis', 'hello world, 
 
 
 
-
+insert into teachers(fullName, lavozim, birth, birthPlace, millat, partiyaviyligi, malumoti, talim, photo) values('Normurodov Elmurod Sodiq o`gli', 'Informatika ustozi', '29.03.1998', 'Qashqadaryo viloyati, Yakkabog` tumani', 'o`zbek', 'yo`q', 'oliy', '2021 y. Toshkent axborot texnologiyalari universiteti (kunduzgi)', 'https://telegra.ph/file/c3e120160ebf00f4f9dc9.jpg');
